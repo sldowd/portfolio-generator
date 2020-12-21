af7b71bb1f1226bbe0afe48c82e86a3d8ddf3ff1
Sarah Dowd <sarahlynnedowd@gmail.com>
2020-12-20 21:23:02 -0500

    added style to page-template

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('Index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output');
-// });
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -140,5 +133,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Portfolio complete! Check out index.html in this directory to see the output');
+    });
   });
